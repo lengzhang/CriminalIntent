56daac19bbeef57cb647ef860a9314bb87dad655
Leng <lengzhang@live.com>
2021-02-12 02:33:32 -0800

    Chapter 8: UI Fragments and the Fragment Manager

--- app/src/main/java/com/lengzhang/android/criminalintent/MainActivity.kt
+++ app/src/main/java/com/lengzhang/android/criminalintent/MainActivity.kt
@@ -2,10 +2,26 @@ package com.lengzhang.android.criminalintent
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+
+private const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val currentFragment = supportFragmentManager.findFragmentById(R.id.fragment_container)
+
+        Log.d(TAG, "1")
+        if (currentFragment == null) {
+            Log.d(TAG, "is null")
+            val fragment = CrimeFragment()
+            supportFragmentManager
+                    .beginTransaction()
+                    .add(R.id.fragment_container, fragment)
+                    .commit()
+        }
+        Log.d(TAG, "2")
     }
 }
