9022f8597b1a07e3acc08507cf8612a4bd576676
Leng <lengzhang@live.com>
2021-02-23 00:39:14 -0800

    passing date from CrimeFragment to DatePickerFragment by arguments

--- app/src/main/java/com/lengzhang/android/criminalintent/DatePickerFragment.kt
+++ app/src/main/java/com/lengzhang/android/criminalintent/DatePickerFragment.kt
@@ -6,10 +6,14 @@ import android.os.Bundle
 import androidx.fragment.app.DialogFragment
 import java.util.*
 
+private const val ARG_DATE = "date"
+
 class DatePickerFragment : DialogFragment() {
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val date = arguments?.getSerializable(ARG_DATE) as Date
         val calendar = Calendar.getInstance()
+        calendar.time = date
         val initialYear = calendar.get(Calendar.YEAR)
         val initialMonth = calendar.get(Calendar.MONTH)
         val initialDay = calendar.get(Calendar.DAY_OF_MONTH)
@@ -21,5 +25,17 @@ class DatePickerFragment : DialogFragment() {
             initialMonth,
             initialDay
         )
+    }
+
+    companion object {
+        fun newInstance(date: Date): DatePickerFragment {
+            val args = Bundle().apply {
+                putSerializable(ARG_DATE, date)
+            }
+
+            return DatePickerFragment().apply {
+                arguments = args
+            }
+        }
     }
 }
