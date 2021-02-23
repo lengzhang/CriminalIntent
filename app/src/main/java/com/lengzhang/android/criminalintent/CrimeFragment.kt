1a2862ea0ac68f3ed200763fada9a3599a35e868
Leng <lengzhang@live.com>
2021-02-22 17:43:44 -0800

    Attaching arguments to a fragment

--- app/src/main/java/com/lengzhang/android/criminalintent/CrimeFragment.kt
+++ app/src/main/java/com/lengzhang/android/criminalintent/CrimeFragment.kt
@@ -10,6 +10,9 @@ import android.widget.Button
 import android.widget.CheckBox
 import android.widget.EditText
 import androidx.fragment.app.Fragment
+import java.util.*
+
+private const val ARG_CRIME_ID = "crime_id"
 
 class CrimeFragment : Fragment() {
 
@@ -24,9 +27,9 @@ class CrimeFragment : Fragment() {
     }
 
     override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_crime, container, false)
 
@@ -48,19 +51,19 @@ class CrimeFragment : Fragment() {
         val titleWatcher = object : TextWatcher {
 
             override fun beforeTextChanged(
-                    sequence: CharSequence?,
-                    start: Int,
-                    count: Int,
-                    after: Int
+                sequence: CharSequence?,
+                start: Int,
+                count: Int,
+                after: Int
             ) {
                 // This space intentionally left blank
             }
 
             override fun onTextChanged(
-                    sequence: CharSequence?,
-                    start: Int,
-                    before: Int,
-                    count: Int
+                sequence: CharSequence?,
+                start: Int,
+                before: Int,
+                count: Int
             ) {
                 crime.title = sequence.toString()
             }
@@ -75,6 +78,18 @@ class CrimeFragment : Fragment() {
         solvedCheckBox.apply {
             setOnCheckedChangeListener { _, isChecked ->
                 crime.isSolved = isChecked
+            }
+        }
+    }
+
+    companion object {
+
+        fun newInstance(crimeId: UUID): CrimeFragment {
+            val args = Bundle().apply {
+                putSerializable(ARG_CRIME_ID, crimeId)
+            }
+            return CrimeFragment().apply {
+                arguments = args
             }
         }
     }
